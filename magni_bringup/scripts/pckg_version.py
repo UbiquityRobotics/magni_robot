23377e5b42a17b6702a478741be74bb9095793ff
Teodor <tp4348@student.uni-lj.si>
2020-08-11 00:26:26 +0200

    Nested packages not working.

--- magni_bringup/scripts/pckg_version.py
+++ magni_bringup/scripts/pckg_version.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
-
+import os
+import rospkg
 import subprocess
 
+rospack = rospkg.RosPack()
 pkgs = ["move_basic", 
 	"fiducials",
 	"ubiquity_motor",
@@ -11,7 +13,17 @@ pkgs = ["move_basic",
 
 print("\n-- UBIQUITY ROBOTICS PACKAGES VERSION --")
 for pkg in pkgs:
-	proc = subprocess.Popen(["rosversion", pkg], stdout=subprocess.PIPE)
+	try:
+		os.chdir(rospack.get_path(pkg))
+	except:
+		print("Package (%s) not found.\n" % pkg)
+		continue
+
+	#  Checks if we are in a Git repository
+	if (subprocess.call(["git", "branch"], stderr=subprocess.STDOUT, stdout=open(os.devnull, 'w')) == 0):
+		proc = subprocess.Popen(["git", "describe", "--tags"], stdout=subprocess.PIPE, stderr=open(os.devnull, 'w'))
+	else: 
+		proc = subprocess.Popen(["rosversion", pkg], stdout=subprocess.PIPE)
+
 	version = proc.communicate()
-	print("%s - %s" % (pkg, version[0].decode('utf-8')))
-	# TODO: Git hash
+	print("(%s) - %s" % (pkg, version[0].decode('utf-8')))
