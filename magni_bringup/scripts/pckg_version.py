768a116e91050947ec2021ff92e8ef9c2cff0e0f
Teodor <tp4348@student.uni-lj.si>
2020-08-18 20:29:23 +0200

    Workaround working.

--- magni_bringup/scripts/pckg_version.py
+++ magni_bringup/scripts/pckg_version.py
@@ -2,19 +2,19 @@
 import os
 import rospkg
 import subprocess
+import xml.etree.ElementTree as ET
 
 rospack = rospkg.RosPack()
 rosstack = rospkg.RosStack()
-pkgs = ["move_basic", 
-	"ubiquity_motor",
-	"fiducials",
-	"magni_robot",
-	"fiducial_slam",
-	"raspicam_node",
-	"dnn_detect"]
+pkgs = ['move_basic', 
+	'ubiquity_motor',
+	'fiducials',
+	'magni_robot',
+	'fiducial_slam',
+	'raspicam_node',
+	'dnn_detect']
 
-print("\n-- UBIQUITY ROBOTICS PACKAGES VERSION --\n\
-----------------------------------------   ")
+print('--- UBIQUITY ROBOTICS PACKAGES VERSION ---\n')
 for pkg in pkgs:
 	try:
 		os.chdir(rospack.get_path(pkg))
@@ -22,10 +22,13 @@ for pkg in pkgs:
 		os.chdir(rosstack.get_path(pkg))
 	finally:
 		#  Checks if we are in a Git repository
-		if (subprocess.call(["git", "branch"], stderr=subprocess.STDOUT, stdout=open(os.devnull, 'w')) == 0):
-			proc = subprocess.Popen(["git", "describe", "--tags"], stdout=subprocess.PIPE, stderr=open(os.devnull, 'w'))
-		else: 
-			proc = subprocess.Popen(["rosversion", pkg], stdout=subprocess.PIPE)
-		# TODO: add option if ros stack is not git repo - read it from the xml.file
-		version = proc.communicate()
-		print("(%s) - %s" % (pkg, version[0].decode('utf-8')))
+		if (subprocess.call(['git', 'branch'], stderr=subprocess.STDOUT, stdout=open(os.devnull, 'w')) == 0):
+			proc = subprocess.Popen(['git', 'describe', '--tags'], stdout=subprocess.PIPE, stderr=open(os.devnull, 'w'))
+		else:
+			proc = subprocess.Popen(['rosversion', pkg], stdout=subprocess.PIPE)
+		version = proc.communicate()[0].rstrip()
+
+		if (version == '<unversioned>' and os.path.isfile('package.xml')):
+			tree = ET.parse('package.xml').getroot()	
+			version = tree.find('version').text
+		print("(%s) - %s" % (pkg, version))
